1d7599609ea4b0811e378ce41d576b9a988b2aab
bencagri <selam@bencagri.com>
2019-07-17 18:36:28 +0300

    remove render from user repository

--- app/repositories/user.go
+++ app/repositories/user.go
@@ -1,7 +1,6 @@
 package repositories
 
 import (
-	"github.com/luci/go-render/render"
 	"github.com/revel/revel"
 	"sozluk/app"
 	"sozluk/app/models"
@@ -14,7 +13,6 @@ func (this UserR) Create(u models.UserM) (*models.UserM, error) {
 	var err error
 
 	if err := app.DB.Create(&u).Error; err != nil {
-		revel.AppLog.Debug(render.Render(u))
 		revel.AppLog.Crit(err.Error())
 	}
 
