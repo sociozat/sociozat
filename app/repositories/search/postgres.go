e1fb72a38a5afaa6c807f852c0007759b7091738
Çağrı S <selam@bencagri.com>
2019-11-07 03:27:16 +0300

    disable debug for postgres query

--- app/repositories/search/postgres.go
+++ app/repositories/search/postgres.go
@@ -12,7 +12,7 @@ func (s PostgresSearch) Query(term string) []models.SiteSearchResult {
 
 	results := []models.SiteSearchResult{}
 
-	if err := app.DB.Debug().Table("searches").Where("title @@ to_tsquery(?)", term).Find(&results).Error; err != nil {
+	if err := app.DB.Table("searches").Where("title @@ to_tsquery(?)", term).Find(&results).Error; err != nil {
 		return results
 	}
 
