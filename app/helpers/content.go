0854759a1babfe189cde847bf23b9c178687100c
bencagri <selam@bencagri.com>
2019-11-22 13:59:44 +0200

    hotfix for multiple url patterns format

--- app/helpers/content.go
+++ app/helpers/content.go
@@ -22,14 +22,16 @@ func ExtractUrl(str string) string {
 	//pattern "<sample.com>#sample.com#"
 	var regex = "<(.*?)>+#(.*?)#"
 	re := regexp.MustCompile(regex)
-	matched := re.FindStringSubmatch(str)
-
+	matched := re.FindAllStringSubmatch(str, -1)
 	if len(matched) > 0 {
-		href := matched[1]
-		url := matched[2]
-		text := fmt.Sprintf(`<a class="item" href="%s" target="_blank">%s <i class="icon external alternate"></i></a>`, href, url)
-		//replace
-		str = re.ReplaceAllString(str, text)
+		for i, _ := range matched {
+			href := matched[i][1]
+			url := matched[i][2]
+			text := fmt.Sprintf(`<a class="item" href="%s" target="_blank">%s <i class="icon external alternate"></i></a>`, href, url)
+
+			//replace
+			str = strings.Replace(str, matched[i][0], text, len(matched))
+		}
 	}
 
 	str = strings.ToLower(str)
