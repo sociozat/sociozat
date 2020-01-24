e90d98033110c2724e6a8ddaaadb67d795c5aee8
Burak <burakalbayrak@live.com>
2020-01-24 19:36:05 +0300

    change password view and lang files

--- app/controllers/settings.go
+++ app/controllers/settings.go
@@ -42,3 +42,34 @@ func (c Settings) SettingsPost() revel.Result {
 
 	return c.Redirect(Settings.View)
 }
+
+func (c Settings) Password() revel.Result {
+	u := c.connected()
+
+	if u == nil {
+		c.Flash.Error(c.Message("auth.login.required "))
+		c.Redirect(User.Login)
+	}
+
+	settings, _ := c.Session.Get("settings")
+	uuid := u.UserID
+	return c.Render(settings, uuid)
+}
+
+func (c Settings) PasswordPost() revel.Result {
+	//prepare settings model
+	u := c.connected()
+	settings := c.SettingsService.TransformValues(c.Params)
+
+	//save
+	_, err := c.SettingsService.Save(u, settings)
+	if err != nil {
+		c.Log.Error("%v", err)
+		c.Flash.Error(c.Message("settings.updated.error"))
+	} else {
+		//override session values
+		c.Flash.Success(c.Message("settings.password.successfully"))
+	}
+
+	return c.Redirect(Settings.View)
+}
